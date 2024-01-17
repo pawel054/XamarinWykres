f8591ddbeb9e9a457e8a3f283cea33de356cecb5
uczen <uczen@mail.com>
2024-01-17 10:24:37 +0100

    dodaj widok xaml wykresu słupkowego

--- Wykresy/Wykresy/App.xaml.cs
+++ Wykresy/Wykresy/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Wykresy
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
