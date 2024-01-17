c74fa688dbb096ac93ea265de2054db5ee3bba59
uczen <uczen@mail.com>
2024-01-17 11:24:29 +0100

    Dodaj obsługę przycisku do edycji danych

--- Wykresy/Wykresy/EditPage.xaml.cs
+++ Wykresy/Wykresy/EditPage.xaml.cs
@@ -15,13 +15,32 @@ namespace Wykresy
         public EditPage()
         {
             InitializeComponent();
+            for (int i = 0; i < MainPage.DaneWykres.Count; i++)
+            {
+                (gridDane.Children[i] as Entry).Text = MainPage.DaneWykres[i].Name;
+                (gridDane.Children[i+7] as Entry).Text = MainPage.DaneWykres[i].Value.ToString();
+            }
 
-
+            entryTitle.Text = MainPage.Tytul;
         }
 
         private void BtnClicked(object sender, EventArgs e)
         {
+            MainPage.DaneWykres = new List<DaneWykres>();
 
+            for (int i = 0; i < 7; i++)
+            {
+                if (!string.IsNullOrWhiteSpace((gridDane.Children[i] as Entry).Text) && !string.IsNullOrWhiteSpace((gridDane.Children[i+7] as Entry).Text)) 
+                    MainPage.DaneWykres.Add(new DaneWykres((gridDane.Children[i] as Entry).Text, double.Parse((gridDane.Children[i+7] as Entry).Text)));
+            }
+
+            if (string.IsNullOrWhiteSpace(entryTitle.Text))
+                DisplayAlert("Alert", "Podano zły tytuł!", "OK");
+            else
+            {
+                MainPage.Tytul = entryTitle.Text;
+                Navigation.PopAsync();
+            }
         }
     }
 }
